6f124fad78dfbff6c6beaa795886a5acdfe9df6f
Lukas Wiegeler <lukas.w6@gso.schule.koeln>
2022-12-11 14:47:54 +0100

    Aufgabe eins und zwei fertig gemacht

--- FuhrparkverwaltungTests/Auto.cs
+++ FuhrparkverwaltungTests/Auto.cs
@@ -8,5 +8,32 @@ namespace FuhrparkverwaltungTests
 {
     class Auto
     {
+        private int kilometerstand;
+
+        public Auto (int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+
+        public int Kilometerstand
+        { 
+            
+            get
+            { 
+                return kilometerstand; 
+            } 
+        }
+
+        public void Fahren (int streckeInKilometern)
+        {
+            if (streckeInKilometern > 0)
+            {
+                kilometerstand += streckeInKilometern;
+            }
+            
+        }
+
+        
+
     }
 }
